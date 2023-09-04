ac4dce9d34158eea5ecfa8b24ce85f849ee8a7f6
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-04 19:32:08 +0100

    pep 8 style

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,6 +5,14 @@ A method that calculate fewest number of operations
 
 
 def minOperations(n):
+    """
+    Given a number n, write a method that calculates
+    the fewest number of operations needed to result
+    in exactly n H characters in the file.
+
+    Returns an integer
+    If n is impossible to achieve, return 0
+    """
     if n <= 1:
         return 0
 
