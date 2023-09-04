5447edb6547300beb7a55871176b2bb999939385
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-04 19:18:39 +0100

    minimum operations method

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""
+A method that calculate fewest number of operations
+"""
 
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = i  # Initialize with the maximum possible number of operations
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
