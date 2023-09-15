eabacc3f7108de220106f84d16629cdf420f3546
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-15 20:08:49 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,51 @@
 #!/usr/bin/python3
 """
-Check data set represents a valid UTF-8 encoding
+Data set represents a valid UTF-8 encoding.
 """
 
+
 def validUTF8(data):
-    num_bytes_to_follow = 0
+    """
+    Check data set represents a valid UTF-8 encoding.
 
-    for byte in data:
-        byte = byte & 255  # Ensure only the least significant 8 bits are considered
-        if num_bytes_to_follow == 0:
-            if byte >> 7 == 0b0:
-                num_bytes_to_follow = 0
-            elif byte >> 5 == 0b110:
-                num_bytes_to_follow = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes_to_follow = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes_to_follow = 3
+    Return: `True` if data is a valid UTF-8 encoding, else return `False`.
+
+    A character in UTF-8 can be 1 to 4 bytes long.
+
+    The data set can contain multiple characters.
+
+    The data will be represented by a list of integers.
+
+    Each integer represents 1 byte of data, therefore you only
+    need to handle the 8 least significant bits of each integer.
+    """
+
+    # Initialize a variable to keep track of the number of expected continuation bytes
+    num_continuation_bytes = 0
+
+    # Iterate through each integer in the data list
+    for num in data:
+        # Convert the integer to its binary representation with 8 least significant bits
+        binary_repr = bin(num)[-8:].zfill(8)
+
+        # If we're not expecting any continuation bytes, this should start with '0'
+        if num_continuation_bytes == 0:
+            if binary_repr[0] == '0':
+                continue  # Single-byte character, move to the next integer
+            elif binary_repr.startswith('110'):
+                num_continuation_bytes = 1  # Expecting 1 continuation byte
+            elif binary_repr.startswith('1110'):
+                num_continuation_bytes = 2  # Expecting 2 continuation bytes
+            elif binary_repr.startswith('11110'):
+                num_continuation_bytes = 3  # Expecting 3 continuation bytes
             else:
-                return False
+                return False  # Invalid leading bits
+
+        # If we're expecting continuation bytes, the integer should start with '10'
         else:
-            if byte >> 6 != 0b10:
-                return False
-            num_bytes_to_follow -= 1
+            if not binary_repr.startswith('10'):
+                return False  # Invalid continuation byte
+            num_continuation_bytes -= 1
 
-    return num_bytes_to_follow == 0
-
-# Test cases
-data = [65]
-print(validUTF8(data))  # Output: True
-
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))  # Output: True
-
-data = [229, 65, 127, 256]
-print(validUTF8(data))  # Output: False
+    # After processing all integers, num_continuation_bytes should be 0
+    return num_continuation_bytes == 0
