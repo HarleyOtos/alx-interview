a3037ff43d40bb10c7db9e84f09f6a22e85ff322
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-16 21:25:16 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,16 +10,25 @@ def canUnlockAll(boxes):
     The first box boxes[0] is unlocked
     Return True if all boxes can be opened, else return False
     """
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = [0]
-
-    while keys:
-        box = keys.pop()
-        for key in boxes[box]:
-            if not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
-
-    return all(unlocked)
+    # Initialize a list to keep track of visited boxes
+    visited = [False] * len(boxes)
+    
+    # Start with the first box (box 0) which is already unlocked
+    visited[0] = True
+    
+    # Create a queue to perform BFS
+    queue = [0]
+    
+    # Perform BFS
+    while queue:
+        current_box = queue.pop(0)
+        
+        # Check the keys in the current box
+        for key in boxes[current_box]:
+            # If the key corresponds to an unvisited box, mark it as visited
+            if not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    
+    # If all boxes have been visited, return True
+    return all(visited)
