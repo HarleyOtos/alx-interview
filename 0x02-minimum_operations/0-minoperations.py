8feed687d4080acea97f8e08782238bfcd2be00d
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-11-06 11:15:23 +0100

    code rewritten due to error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,15 +14,15 @@ def minOperations(n):
     Returns an integer
     If n is impossible to achieve, return 0
     """
-    if n <= 1:
+    if not isinstance(n, int):
         return 0
 
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        dp[i] = i
-        for j in range(2, i // 2 + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n]
+    op = 0
+    i = 2
+    while (i <= n):
+        if not (n % i):
+            n = int(n / i)
+            op += i
+            i = 1
+        i += 1
+    return op
